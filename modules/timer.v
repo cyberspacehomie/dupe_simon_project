a5e00358b4138dee9b09d316361c698cea96a0a3
Isaac <isaaclouistag@gmail.com>
2024-11-27 09:13:56 -0800

    cleaned up timer

--- modules/timer.v
+++ modules/timer.v
@@ -11,9 +11,8 @@ end
 always @(posedge clk) begin
     counter = counter - 1;
     if ((counter == 0) || (reset)) counter = time;
-
 end
-// basically this should take a target value (time) 
-// count up to it, output pulse when done 
+// basically this should load a counter with a target value (time input) 
+// count down from it to 0, output pulse when it hits 0, and then reset itself 
 
 endmodule
