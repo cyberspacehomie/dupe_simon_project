e0e221419d44a9340b6c0c07bad5fe2d00d48d11
Isaac <isaaclouistag@gmail.com>
2024-11-27 09:10:18 -0800

    finished? timer module

--- modules/timer.v
+++ modules/timer.v
@@ -1,14 +1,16 @@
 module timer (output reg pulse,
 input clk, input reset, input [24:0] time);
 
-reg [25;0] counter;
+reg [25:0] counter;
 
 always @* begin
-
+    if (counter == 0) pulse = 1;
+    if (reset) pulse = 0;
 end
 
 always @(posedge clk) begin
-    counter = counter + 1;
+    counter = counter - 1;
+    if ((counter == 0) || (reset)) counter = time;
 
 end
 // basically this should take a target value (time) 
