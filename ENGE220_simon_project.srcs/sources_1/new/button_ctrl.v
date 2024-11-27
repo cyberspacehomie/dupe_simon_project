1efdbb9e3f5278386b9b8c54fefc85d227564b19
Daniel <159750540+WaffleHouseDevOpsTeam@users.noreply.github.com>
2024-11-26 19:10:45 -0800

    stuff

--- ENGE220_simon_project.srcs/sources_1/new/button_ctrl.v
+++ ENGE220_simon_project.srcs/sources_1/new/button_ctrl.v
@@ -5,11 +5,13 @@ module button_ctrl(
     );
    
     always @* begin
+			if (enable) begin
         case (button_in)
             4'b0001: button_loc = 2'b00;
             4'b0010: button_loc = 2'b01;
             4'b0100: button_loc = 2'b10;
             4'b1000: button_loc = 2'b11;
         endcase
+			end
     end 
 endmodule
